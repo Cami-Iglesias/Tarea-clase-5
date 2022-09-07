d88230467f21682bed16c3733fd81796b544c201
Cami-Iglesias <camila.iglesias854@gmail.com>
2022-09-07 11:36:07 -0300

    hoja de estilo
    es la hoja de estilo para todas las tareas

--- tarea-1.js
+++ tarea-1.js
@@ -1,7 +1,11 @@
+//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
+//cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
+// en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
+
 const botonCalcular = document.querySelector('#boton-calcular')
 
 botonCalcular.onclick = function () {
-   const salarioAnual= Number(document.querySelector("#salario-anual").value);
+   const salarioAnual = Number(document.querySelector("#salario-anual").value);
    const salarioMensual = calcularSalarioMensual(salarioAnual);
    
    document.querySelector('#salario-mensual').value = salarioMensual;
@@ -9,5 +13,6 @@ botonCalcular.onclick = function () {
    return false;
 }
 function calcularSalarioMensual (salarioAnual) {
-    return salarioAnual / 12;
+   const mesesDelAnio = 12;
+   return salarioAnual / mesesDelAnio;
     }
